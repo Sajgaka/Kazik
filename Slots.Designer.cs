888ac98e18f83c1327591d151e9260a0e9baa614
Ken <Ken@DESKTOP-9FAA356>
2024-04-23 05:21:43 -0700

    Added timers to the three slots on the form "Slots".

--- Slots.Designer.cs
+++ Slots.Designer.cs
@@ -28,37 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Slots));
+            this.pictureBoxMiddleSlot = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRightSlot = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLeftSlot = new System.Windows.Forms.PictureBox();
+            this.imageListLeftSlot = new System.Windows.Forms.ImageList(this.components);
+            this.imageListMiddleSlot = new System.Windows.Forms.ImageList(this.components);
+            this.imageListRightSlot = new System.Windows.Forms.ImageList(this.components);
+            this.timerLeftSlot = new System.Windows.Forms.Timer(this.components);
+            this.timerMiddleSlot = new System.Windows.Forms.Timer(this.components);
+            this.timerRightSlot = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMiddleSlot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRightSlot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeftSlot)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBoxMiddleSlot
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(574, 365);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxMiddleSlot.Location = new System.Drawing.Point(574, 365);
+            this.pictureBoxMiddleSlot.Name = "pictureBoxMiddleSlot";
+            this.pictureBoxMiddleSlot.Size = new System.Drawing.Size(150, 150);
+            this.pictureBoxMiddleSlot.TabIndex = 0;
+            this.pictureBoxMiddleSlot.TabStop = false;
             // 
-            // pictureBox2
+            // pictureBoxRightSlot
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(755, 365);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pictureBoxRightSlot.Location = new System.Drawing.Point(755, 365);
+            this.pictureBoxRightSlot.Name = "pictureBoxRightSlot";
+            this.pictureBoxRightSlot.Size = new System.Drawing.Size(150, 150);
+            this.pictureBoxRightSlot.TabIndex = 1;
+            this.pictureBoxRightSlot.TabStop = false;
             // 
-            // pictureBox3
+            // pictureBoxLeftSlot
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(392, 365);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.pictureBoxLeftSlot.Location = new System.Drawing.Point(392, 365);
+            this.pictureBoxLeftSlot.Name = "pictureBoxLeftSlot";
+            this.pictureBoxLeftSlot.Size = new System.Drawing.Size(150, 150);
+            this.pictureBoxLeftSlot.TabIndex = 2;
+            this.pictureBoxLeftSlot.TabStop = false;
+            // 
+            // imageListLeftSlot
+            // 
+            this.imageListLeftSlot.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListLeftSlot.ImageStream")));
+            this.imageListLeftSlot.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListLeftSlot.Images.SetKeyName(0, "Cherry.jpg");
+            this.imageListLeftSlot.Images.SetKeyName(1, "Pear.jpg");
+            this.imageListLeftSlot.Images.SetKeyName(2, "Seven.jpg");
+            this.imageListLeftSlot.Images.SetKeyName(3, "Star.jpg");
+            this.imageListLeftSlot.Images.SetKeyName(4, "Strawberry.jpg");
+            // 
+            // imageListMiddleSlot
+            // 
+            this.imageListMiddleSlot.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListMiddleSlot.ImageStream")));
+            this.imageListMiddleSlot.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListMiddleSlot.Images.SetKeyName(0, "Cherry.jpg");
+            this.imageListMiddleSlot.Images.SetKeyName(1, "Pear.jpg");
+            this.imageListMiddleSlot.Images.SetKeyName(2, "Seven.jpg");
+            this.imageListMiddleSlot.Images.SetKeyName(3, "Star.jpg");
+            this.imageListMiddleSlot.Images.SetKeyName(4, "Strawberry.jpg");
+            // 
+            // imageListRightSlot
+            // 
+            this.imageListRightSlot.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListRightSlot.ImageStream")));
+            this.imageListRightSlot.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListRightSlot.Images.SetKeyName(0, "Cherry.jpg");
+            this.imageListRightSlot.Images.SetKeyName(1, "Pear.jpg");
+            this.imageListRightSlot.Images.SetKeyName(2, "Seven.jpg");
+            this.imageListRightSlot.Images.SetKeyName(3, "Star.jpg");
+            this.imageListRightSlot.Images.SetKeyName(4, "Strawberry.jpg");
+            // 
+            // timerLeftSlot
+            // 
+            this.timerLeftSlot.Enabled = true;
+            this.timerLeftSlot.Tick += new System.EventHandler(this.timerLeftSlot_Tick);
+            // 
+            // timerMiddleSlot
+            // 
+            this.timerMiddleSlot.Enabled = true;
+            this.timerMiddleSlot.Tick += new System.EventHandler(this.timerMiddleSlot_Tick);
+            // 
+            // timerRightSlot
+            // 
+            this.timerRightSlot.Enabled = true;
+            this.timerRightSlot.Tick += new System.EventHandler(this.timerRightSlot_Tick);
             // 
             // Slots
             // 
@@ -66,22 +119,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Kazik.Properties.Resources.Slots2;
             this.ClientSize = new System.Drawing.Size(1274, 778);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxLeftSlot);
+            this.Controls.Add(this.pictureBoxRightSlot);
+            this.Controls.Add(this.pictureBoxMiddleSlot);
             this.Name = "Slots";
             this.Text = "Slots";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMiddleSlot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRightSlot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeftSlot)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBoxMiddleSlot;
+        private System.Windows.Forms.PictureBox pictureBoxRightSlot;
+        private System.Windows.Forms.PictureBox pictureBoxLeftSlot;
+        private System.Windows.Forms.ImageList imageListLeftSlot;
+        private System.Windows.Forms.ImageList imageListMiddleSlot;
+        private System.Windows.Forms.ImageList imageListRightSlot;
+        private System.Windows.Forms.Timer timerLeftSlot;
+        private System.Windows.Forms.Timer timerMiddleSlot;
+        private System.Windows.Forms.Timer timerRightSlot;
     }
 }
--- Slots.cs
+++ Slots.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,48 @@ namespace Kazik
         public Slots()
         {
             InitializeComponent();
+        }
+
+        int intLeftImgNum = 0;
+        private void timerLeftSlot_Tick(object sender, EventArgs e)
+        {
+            pictureBoxLeftSlot.Image = imageListLeftSlot.Images[intLeftImgNum];
+            if (intLeftImgNum == imageListLeftSlot.Images.Count - 1)
+            {
+                intLeftImgNum = 0;
+            }
+            else
+            {
+                intLeftImgNum++;
+            }
+        }
+
+        int intMiddleImgNum = 0;
+        private void timerMiddleSlot_Tick(object sender, EventArgs e)
+        {
+            pictureBoxMiddleSlot.Image = imageListMiddleSlot.Images[intMiddleImgNum];
+            if (intMiddleImgNum == imageListMiddleSlot.Images.Count - 1)
+            {
+                intMiddleImgNum = 0;
+            }
+            else
+            {
+                intMiddleImgNum++;
+            }
+        }
+
+        int intRightImgNum = 0;
+        private void timerRightSlot_Tick(object sender, EventArgs e)
+        {
+            pictureBoxRightSlot.Image = imageListRightSlot.Images[intRightImgNum];
+            if (intRightImgNum == imageListRightSlot.Images.Count - 1)
+            {
+                intRightImgNum = 0;
+            }
+            else
+            {
+                intRightImgNum++;
+            }
         }
     }
 }
